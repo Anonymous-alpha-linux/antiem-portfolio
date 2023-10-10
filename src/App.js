6f3dccd42a406b1037de93f9a40e0f9ee00a223b
Anonymous-alpha-linux <tinhntgcd18753@fpt.edu.vn>
2023-10-10 08:59:43 +0700

    tinh - add router name

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import { RouterProvider } from 'react-router-dom';
 
 import { routers } from './router';
 
+// css
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 function App() {
     return <RouterProvider router={routers}></RouterProvider>;
 }
--- src/layout/sidebar/sidebar.js
+++ src/layout/sidebar/sidebar.js
@@ -1,11 +1,34 @@
 import React from 'react';
+import { Col, Row } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
-function Sidebar() {
+function Sidebar({ children, menu }) {
     return (
-        <div>
-            <div>Sidebar</div>
-            <div></div>
-        </div>
+        <Row>
+            <Col lg={3}>
+                <ul>
+                    {menu.map((item, index) => {
+                        const { link, name, ...restItem } = item;
+                        return (
+                            <li key={index}>
+                                <Link to={link}>{name}</Link>
+                                <ul>
+                                    {restItem?.subs?.map((subItem, index2) => {
+                                        const { link, name } = subItem;
+                                        return (
+                                            <li key={index2}>
+                                                <Link to={link}>{name}</Link>
+                                            </li>
+                                        );
+                                    })}
+                                </ul>
+                            </li>
+                        );
+                    })}
+                </ul>
+            </Col>
+            <Col lg={9}>{children}</Col>
+        </Row>
     );
 }
 
